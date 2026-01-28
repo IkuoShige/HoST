76107f55a4cebd31b0e9061fc3f6d1921e52bf87
IkuoShige <shigeikuo@gmail.com>
2026-01-28 15:54:17 +0900

    build: Update uv setup script and Docker environment

---
+++ launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export DISPLAY=:1
+xhost +
+docker run --rm -it --gpus all --runtime=nvidia \
+  -e DISPLAY=$DISPLAY \
+  -e NVIDIA_DRIVER_CAPABILITIES=all \
+  -e NVIDIA_VISIBLE_DEVICES=all \
+  -e "ACCEPT_EULA=Y" \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  --name host \
+  host:latest
+
+  #--entrypoint bash \
